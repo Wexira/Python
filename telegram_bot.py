71b2ba0f8b248bdd4a529145bdcba37c6fed10f9
Lana <svetlanawex@gmail.com>
2020-01-10 16:09:53 +0400

    telegram bot not work

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,31 @@
+# Добланутый бот
 
+import pyowm
+from py_telegram_bot.bot import Bot
 
-import bot
-
+owm = pyowm.OWM('49877e0901f8715065b4e76841918c56', language="ru")
 bot = Bot('1001825359:AAHBZpu6byz6BzJGXn1uzOk2DXukMo0vLLw')
+api = bot.get_api()
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-    # bot.reply_to(message, message.text)
-    bot.send_message(message.chat.id, message.text)
+    observation = owm.weather_at_place(place)
+    w = observation.get_weather()
+    temp = w.get_temperature('celsius')["temp"] 
+    
+    answer = "В городе " + place + " сейчас " + w.get_detailed_status() + "\n"
+    answer += "Температура сейчас в районе " + str(temp) + "\n\n"
 
-    bot.polling( none_stop = True )
+    if temp < 10:
+        answer += "Сейчас ппц как холодно, одевайся как танк!"
+    elif temp < 20:
+        answer += "Сейчас холодно, оденься потеплее."
+    else:
+        answer += "Температура норм, одевай что угодно"
+
+    bot.send_message(message.chat.id, answer)
+
+bot.polling( none_stop = True )
+
+
+
